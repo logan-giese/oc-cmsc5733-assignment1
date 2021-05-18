b216355087e275c2945579c723a581dad61dea57
Logan Giese <logan.giese@eagles.oc.edu>
2021-05-17 21:55:17 -0600

    Add number list multiplication

--- NumberListSquasher.cs
+++ NumberListSquasher.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace OC_CMSC_5733_Assignment_1
 {
-    // Squashes a list of numbers into a single value via addition
+    // Squashes a list of numbers into a single value via addition or multiplication
     class NumberListSquasher
     {
         static void Main(string[] args)
@@ -11,9 +11,10 @@ namespace OC_CMSC_5733_Assignment_1
             // Test data
             int[] nums = { 3, 4, 5, 7, 8, 9, 42 }; // Sum should be 78, product should be 1,270,080
 
-            // Test summation
+            // Run tests
             Console.WriteLine("== Number List Squasher ==\n");
-            Console.WriteLine("List sum = " + Sum(nums));
+            Console.WriteLine("List sum\t= " + Sum(nums));
+            Console.WriteLine("List product\t= " + Product(nums));
         }
 
         /// <summary>
@@ -27,6 +28,19 @@ namespace OC_CMSC_5733_Assignment_1
                 sum += i;
             }
             return sum;
+        }
+
+        /// <summary>
+        /// Find the product of a list of numbers
+        /// </summary>
+        static int Product(int[] numbers)
+        {
+            int product = numbers.Length > 0 ? numbers[0] : 0;
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                product *= numbers[i];
+            }
+            return product;
         }
     }
 }
