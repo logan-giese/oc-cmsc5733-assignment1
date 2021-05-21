ac1262babc1ea25637d829c1650a3d2657bd28ad
logieg <xlr8speedie@gmail.com>
2021-05-20 19:07:23 -0500

    Setup for number add checker

---
+++ NumberAddChecker.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace OC_CMSC_5733_Assignment_1
+{
+    // Class with verification methods for addition of number lists
+    class NumberAddChecker
+    {
+		// Checks whether some two numbers in "numbers" can be added to get "target"
+        static bool CanAddTo(int[] numbers, int target)
+		{
+			
+		}
+    }
+}
---
+++ TestNumberAddChecker.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.QualityTools.UnitTestFramework;
+
+namespace OC_CMSC_5733_Assignment_1
+{
+    // Class to test that NumberAddChecker works properly
+    [TestClass]
+	public class TestNumberAddChecker
+    {
+		[TestMethod]
+		public void TestPositiveOnly() {
+			// TODO
+		}
+    }
+}
