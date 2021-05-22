294f9dc9047f82c7d624c277659e33da81405265
Logan Giese <logan.giese@eagles.oc.edu>
2021-05-21 18:30:56 -0600

    Properly set up the testing project

---
+++ Testing/NumberAddTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OC_CMSC_5733_Assignment_1;
+
+namespace Testing
+{
+    [TestClass]
+    public class NumberAddTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
