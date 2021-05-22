fbdd5ff56f3efeda2d8f08a480f3942a64e8f647
Logan Giese <logan.giese@eagles.oc.edu>
2021-05-21 19:04:23 -0600

    Implement number add checker
    Checks if some two numbers in an array can be added to get the target value

--- NumberAddChecker.cs
+++ NumberAddChecker.cs
@@ -4,12 +4,22 @@ using System.Collections.Generic;
 namespace OC_CMSC_5733_Assignment_1
 {
     // Class with verification methods for addition of number lists
-    class NumberAddChecker
+    public class NumberAddChecker
     {
 		// Checks whether some two numbers in "numbers" can be added to get "target"
-        static bool CanAddTo(int[] numbers, int target)
+        public static bool CanAddTo(int[] numbers, int target)
 		{
-			
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    int a = numbers[i];
+                    int b = numbers[j];
+                    if ((a + b) == target)
+                        return true;
+                }
+            }
+            return false;
 		}
     }
 }
