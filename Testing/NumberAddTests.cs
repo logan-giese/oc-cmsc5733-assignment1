aa7b2c3bc5593ea9e761fc81c024392448d9629f
Logan Giese <logan.giese@eagles.oc.edu>
2021-05-21 19:05:50 -0600

    Add unit tests for number add checker
    Also fix build issues related to nested project files

--- Testing/NumberAddTests.cs
+++ Testing/NumberAddTests.cs
@@ -7,9 +7,73 @@ namespace Testing
     public class NumberAddTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void PositiveOnly()
         {
-            
+            // Arrange
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int target = 14;
+
+            // Act
+            bool canAdd = NumberAddChecker.CanAddTo(numbers, target);
+
+            // Assert
+            Assert.AreEqual(true, canAdd);
+        }
+
+        [TestMethod]
+        public void NegativeOnly()
+        {
+            // Arrange
+            int[] numbers = { -1, -2, -3, -4, -5, -6, -7, -8 };
+            int target = -13;
+
+            // Act
+            bool canAdd = NumberAddChecker.CanAddTo(numbers, target);
+
+            // Assert
+            Assert.AreEqual(true, canAdd);
+        }
+
+        [TestMethod]
+        public void NegativeInputPositiveTarget()
+        {
+            // Arrange
+            int[] numbers = { 1, 2, -3, 4, -5, 6 };
+            int target = 1;
+
+            // Act
+            bool canAdd = NumberAddChecker.CanAddTo(numbers, target);
+
+            // Assert
+            Assert.AreEqual(true, canAdd);
+        }
+
+        [TestMethod]
+        public void NegativeInputNegativeTarget()
+        {
+            // Arrange
+            int[] numbers = { 1, 2, -3, 4, -5, 6 };
+            int target = -3;
+
+            // Act
+            bool canAdd = NumberAddChecker.CanAddTo(numbers, target);
+
+            // Assert
+            Assert.AreEqual(true, canAdd);
+        }
+
+        [TestMethod]
+        public void LargeInput()
+        {
+            // Arrange
+            int[] numbers = { 279, 1230, 503, 458, 1555 };
+            int target = 1834;
+
+            // Act
+            bool canAdd = NumberAddChecker.CanAddTo(numbers, target);
+
+            // Assert
+            Assert.AreEqual(true, canAdd);
         }
     }
 }
